117a1300226a7cc03088ece1bf9e6388f50ec2f8
GhostGamesV11 <47499005+GhostGamesV11@users.noreply.github.com>
2021-05-16 12:37:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ print(df.loc[df["Imie"]=="BARTOSZ"])
 print("\n")
 print(df["Liczba"].sum())
 print("\n")
-print(df.loc[(df["Rok"]<= 2005)&(df["Rok"]>=2000),"Liczba"].sum())
+print(df.loc[(df["Rok"]<=2005)&(df["Rok"]>=2000),"Liczba"].sum())
 print("\n")
 print("Chlopcy =",df.loc[df["Plec"]=="M","Liczba"].sum())
-print("Dziewczyny =",df.loc[df["Plec"] == "K","Liczba"].sum())
+print("Dziewczyny =",df.loc[df["Plec"]=="K","Liczba"].sum())
 print("\n")
 ch=df.loc[df["Plec"] == "M"]
 sumach=ch.loc[ch.groupby("Rok")["Liczba"].idxmax()]
-dzw=df.loc[df["Plec"] == "K"]
+dzw=df.loc[df["Plec"]=="K"]
 sumadzw=dzw.loc[dzw.groupby("Rok")["Liczba"].idxmax()]
 chidzw=pd.concat([sumach, sumadzw])
 print(chidzw.sort_values(by="Rok"))
